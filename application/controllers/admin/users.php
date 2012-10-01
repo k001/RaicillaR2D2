9364ac2f3c17f6e5e5f4ab6ba0b01ed2c6c555c3
Ivan Zenteno <k001.operator@gmail.com>
2012-10-01 09:29:01 -0500

    Adding controller user and model

---
+++ application/controllers/admin/users.php
@@ -0,0 +1,22 @@
+<?php
+
+class Users extends CI_Controller{
+	
+	public function __construct(){
+		parent::__construt();
+		$this->load->]model('users_model');
+	}	
+
+	public function index(){
+
+	}
+
+	public function login(){
+		
+	}
+
+	public function logout(){
+
+	}
+
+}
