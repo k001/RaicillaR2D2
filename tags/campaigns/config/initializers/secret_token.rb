7c3640f7464f168d39f67f2710b08a9e722e7d95
Ivan Zenteno <ivan.zenteno@infapen.com>
2012-10-09 15:47:08 -0500

    Adding campaings

---
+++ tags/campaigns/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campaigns::Application.config.secret_token = '1d1697f24dfa41dc4161a830741830954cb1f022f3cda337a56553bb900e877d7ac6900c7107a22d4bfc2ebde4ac911854261e25724e79d30b3341452a1f5771'
